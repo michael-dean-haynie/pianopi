8cea26f27cb6d1ec2597590a324b240cfb1d5e21
Michael Haynie <michael.dean.haynie@gmail.com>
2023-09-23 17:29:20 -0700

    start listening for midi devices

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+const { WebMidi } = require("webmidi")
+
 console.log('Hello from Pianopi!')
+
+WebMidi
+    .enable()
+    .then(onEnabled)
+    .catch(err => alert(err));
+
+function onEnabled() {
+    // Inputs
+    WebMidi.inputs.forEach(input => console.log(input.manufacturer, input.name));
+
+    // Outputs
+    WebMidi.outputs.forEach(output => console.log(output.manufacturer, output.name));
+
+}
