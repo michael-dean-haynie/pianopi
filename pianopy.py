72f2a6912c36d87509baaf91abf948363860868c
Michael Haynie <michael.dean.haynie@gmail.com>
2023-09-24 21:14:36 -0700

    simplify script for troubleshooting

--- pianopy.py
+++ pianopy.py
@@ -12,32 +12,33 @@ def on_message(wsapp, message):
     print(message)
 
 def main():
-    # Connect to websocket
-    ws = websocket.WebSocket()
-    ws.connect("wss://inalltwelvekeys.com")
-    ws.send("Hello, from python")
-
-    # List available MIDI input ports
-    list_midi_input_ports()
-
-    # Prompt the user to select a MIDI input port
-    selected_port_index = int(input("Enter the number of the MIDI input port you want to use: ")) - 1
-    input_ports = mido.get_input_names()
-
-    try:
-        # Open the selected MIDI input port
-        with mido.open_input(input_ports[selected_port_index]) as midi_in:
-            print(f"Connected to {input_ports[selected_port_index]}...")
-
-            # Start receiving and printing MIDI events
-            for message in midi_in:
-                print(f"Received: {message}")
-                ws.send(f"Received: {message}")
-
-    except KeyboardInterrupt:
-        print("\nExiting...")
-    except OSError as e:
-        print(f"Error: {e}")
+    print("running the script...")
+#     # Connect to websocket
+#     ws = websocket.WebSocket()
+#     ws.connect("wss://inalltwelvekeys.com")
+#     ws.send("Hello, from python")
+#
+#     # List available MIDI input ports
+#     list_midi_input_ports()
+#
+#     # Prompt the user to select a MIDI input port
+#     selected_port_index = int(input("Enter the number of the MIDI input port you want to use: ")) - 1
+#     input_ports = mido.get_input_names()
+#
+#     try:
+#         # Open the selected MIDI input port
+#         with mido.open_input(input_ports[selected_port_index]) as midi_in:
+#             print(f"Connected to {input_ports[selected_port_index]}...")
+#
+#             # Start receiving and printing MIDI events
+#             for message in midi_in:
+#                 print(f"Received: {message}")
+#                 ws.send(f"Received: {message}")
+#
+#     except KeyboardInterrupt:
+#         print("\nExiting...")
+#     except OSError as e:
+#         print(f"Error: {e}")
 
 if __name__ == "__main__":
     main()
