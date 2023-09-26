948249602dd2ed54ba12caeac8cce7b4788eacbf
Michael Haynie <michael.dean.haynie@gmail.com>
2023-09-25 22:56:17 -0700

    use python method instead of aconnect

--- pianopi.py
+++ pianopi.py
@@ -22,29 +22,29 @@ with open(pid_file_path, "w") as pid_file:
 def custom_signal_handler(signum, frame):
     # Your custom logic when the signal is received
     print(f"Received signal {signum}", flush=True)
-    run_aconnect()
+    list_midi_input_ports()
 
-def run_aconnect():
-    # Define the `aconnect` command you want to run
-    aconnect_command = ["aconnect", "-i"]
-
-    try:
-        # Run the `aconnect` command
-        result = subprocess.run(
-            aconnect_command,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            text=True,
-            check=True,
-        )
-
-        # Print the command's standard output
-        print("aconnect output:", flush=True)
-        print(result.stdout, flush=True)
-
-    except subprocess.CalledProcessError as e:
-        # Handle errors, if any
-        print(f"Error running aconnect: {e}")
+# def run_aconnect():
+#     # Define the `aconnect` command you want to run
+#     aconnect_command = ["aconnect", "-i"]
+#
+#     try:
+#         # Run the `aconnect` command
+#         result = subprocess.run(
+#             aconnect_command,
+#             stdout=subprocess.PIPE,
+#             stderr=subprocess.PIPE,
+#             text=True,
+#             check=True,
+#         )
+#
+#         # Print the command's standard output
+#         print("aconnect output:", flush=True)
+#         print(result.stdout, flush=True)
+#
+#     except subprocess.CalledProcessError as e:
+#         # Handle errors, if any
+#         print(f"Error running aconnect: {e}")
 
 # Register the custom signal handler for a specific signal (e.g., SIGUSR1)
 signal.signal(signal.SIGUSR1, custom_signal_handler)
@@ -67,33 +67,6 @@ def on_open(ws):
     print("Opened connection", flush=True)
     # List available MIDI input ports
     list_midi_input_ports()
-
-
-    # Create a udev context
-    context = Context()
-
-    # Create a monitor for udev events
-    monitor = Monitor.from_netlink(context)
-    monitor.filter_by(subsystem='snd')
-
-    # Define a callback function to handle MIDI device events
-    def handle_midi_device_event(action, device):
-        if 'MIDI' in device.get('DEVTYPE', ''):
-            print(f"MIDI Device {device.sys_name} was {action}")
-
-            # Add your custom logic to react to the MIDI device event here
-
-    # Create a monitor observer and attach the callback function
-    observer = MonitorObserver(monitor, callback=handle_midi_device_event, name='monitor-observer')
-    observer.daemon = False  # Set daemon to False to keep the script running
-
-    # Start monitoring
-    observer.start()
-
-    try:
-        observer.join()  # Keep the script running
-    except KeyboardInterrupt:
-        observer.stop()
 
 def main():
     print("running the script... with all the flushes", flush=True)
