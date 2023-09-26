b8136344b05bea87fa26e7e4ccc245919d3d8434
Michael Haynie <michael.dean.haynie@gmail.com>
2023-09-25 22:45:46 -0700

    fix typo with variable in sh file

--- usb-signal.sh
+++ usb-signal.sh
@@ -8,7 +8,7 @@ if [ -f "$pid_file" ]; then
     # Read the PID from the PID file and store it in a variable
     read -r pid < "$pid_file"
     echo "PID read from $pid_file: $pid"
-    sudo kill -USR1 12345
+    sudo kill -USR1 "$pid"
 else
     echo "PID file not found: $pid_file"
 fi
