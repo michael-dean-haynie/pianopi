24dda18bb8f16ae38b03cef6dc89587bd654e4fa
Michael Haynie <michael.dean.haynie@gmail.com>
2023-09-29 11:59:44 -0700

    use subshell for sh compatability (not bash)

--- usb-device.sh
+++ usb-device.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-# Redirect stdout to systemd journal
-sudo exec 1> >(logger -s -t "$(basename "$0")")
-sudo exec 2>&1
+# Redirect stdout to systemd journal using a subshell
+{
+  file="/var/last-usb-device-event.txt"
 
-file="/var/last-usb-device-event.txt"
+  # Check if the PID file exists
+  if [ -f "$file" ]; then
+      echo "touching existing file: $file"
+      sudo touch $file
+  else
+      echo "touching new file: $file"
+      sudo touch $file
+  fi
 
-# Check if the PID file exists
-if [ -f "$file" ]; then
-    echo "touching existing file: $file"
-    sudo touch $file
-else
-    echo "touching new file: $file"
-    sudo touch $file
-fi
+  sudo ping mbp.local -c 1
+} 2>&1 | logger -t "$(basename "$0")"
 
-sudo ping mbp.local -c 1
